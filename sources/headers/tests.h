88c3344a0344238b2e0fc85b265aaa7e02b35786
LashaGOG <gogritchianilasha@gmail.com>
2022-11-22 17:23:45 +0100

    Pour écrire tests

---
+++ sources/headers/tests.h
@@ -0,0 +1,15 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "tools.h"
+
+#ifndef TESTS
+#define TESTS
+
+/* TESTS DE TOOLS.C */
+void test_hexToBin();
+void test_binToHex();
+void test_hexToDec(); 
+
+#endif
---
+++ sources/tests.c
@@ -0,0 +1,39 @@
+#include "./headers/tests.h"
+
+/* on définit les tests pour chaque fonction ici */
+
+void test_hexToBin() {
+    printf("________________________________\n");
+    printf("Test de la fonction hetToBin\n");
+    char *testHexToBin[] = {"0","1","2","3","4","5","6","7","8","9","a","A", "B","b","C","c","D","d","E","e","F","f"}; 
+    for (int i = 0; i < 22; i++) {
+        printf("Nombre en Hexa : %s \n", testHexToBin[i]);
+        printf("Son equivalent en binaire : ");
+        hexToBin(testHexToBin[i]);
+        printf("\n");  
+    }
+    printf("Test de la fonction hexToBin terminé\n"); 
+    printf("________________________________\n");
+}
+void test_binToHex() {
+    printf("________________________________\n");   
+    printf("Test de la fonction binToHex\n");
+    char *testBinToHex[] = {"0000","0001","0010","1111"}; 
+    for (int i = 0; i < 4; i++) {
+        printf("Nombre en bin : %s \n", testBinToHex[i]);
+        printf("Son equivalent en hexa : %X\n", binToHex(testBinToHex[i]));
+    }
+    printf("Test de la fonction binToHex terminé\n");
+    printf("________________________________\n");
+}
+void test_hexToDec() {
+    printf("________________________________\n");
+    printf("Test de la fonction HexToDec\n");
+    char *testHexToDec[] = {"A","1","0","AB","ab","fa","f9"}; 
+    for (int i = 0; i < 7; i++) {
+        printf("Nombre en hex : %s \n", testHexToDec[i]);
+        printf("Son equivalent en decimale : %d\n", hexToDec(testHexToDec[i]));
+    }
+    printf("Test de la fonction hexToDec terminé\n");
+    printf("________________________________\n");
+}
