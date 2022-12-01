c224216a6d97b96f3e3280dafaf539b9cd8b9955
superfiloche <tanphilippe@outlook.com>
2022-12-01 01:06:27 +0100

    modified  separate_chunks

--- sources/http.c
+++ sources/http.c
@@ -4,32 +4,33 @@ char* separate_chunks (char *bytes) {
     char *str = strdup(bytes);
     char tmp[4] = {0};
     tmp[2] = " ";
-    int i = 0, j = 1;
+    int i = 0, j = 1, k = 0;
     char *res = (char *) calloc(2, sizeof(char));
     
     while (str[j+3])
     {
         tmp[0] = str[i];
         tmp[1] = str[j];
-        i += 3;
-        j += 3;
         if (tmp[0] == '0' && tmp[1] == 'd') {
-            tmp[0] = str[i];
-            tmp[1] = str[j];
+            tmp[0] = str[i+3];
+            tmp[1] = str[j+3];
             if (tmp[0] == '0' && tmp[1] == 'a')
                 continue;
-            bytes = bytes[j+3];
+            break;
         }
         else {
-           res = (char *) realloc(res, (strlen(res) + 4) * sizeof(char));
+           res = (char *) realloc(res, (strlen(res) + 3) * sizeof(char));
            res[i] = tmp[0];
            res[j] = tmp[1];
-           res[j + 1] = tmp[2];
         }
-
+        i++;
+        j++;
     }
 
-
+    res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
     free(str);
-    return char*
+
+    //modify byte -> byte[j + 5] if it exists
+    
+    return res;
 }
