b216c0a83eb3532bb3b2366af91c2d6965af9df6
superfiloche <tanphilippe@outlook.com>
2022-12-01 01:31:10 +0100

    get_champ added in http.c

--- sources/http.c
+++ sources/http.c
@@ -28,9 +28,27 @@ char* separate_chunks (char *bytes) {
     }
 
     res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
+    res[strlen(str)] = '\0';
     free(str);
 
     //modify byte -> byte[j + 5] if it exists
-    
+
     return res;
+}
+
+champ *get_champ (char *bytes)
+{
+    int i = 0, j = 1;
+    while(j < (int) strlen(bytes))
+    {
+        if (bytes[i] == '0' && bytes[j] == '2')
+        {
+            char *entete = strndup(&bytes[0], (i - 1));
+            char *value = strndup(&bytes[j+2], (strlen(bytes) - (j + 3)));
+            break;
+        }
+        i++;
+        j++;
+    }  
+    return create_champ(entete, value);
 }
