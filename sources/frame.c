48c9513b9be41105b09296320cf8b044aa81d1da
superfiloche <tanphilippe@outlook.com>
2022-12-07 01:37:57 +0100

    frame.c to test

--- sources/frame.c
+++ sources/frame.c
@@ -75,30 +75,13 @@ void print_frame(frame *ptr)
 char *filter(frame *ptr)
 {
     frame *tmp = ptr;
-    char *syntax = "Before typing your filter, here are few rules to respect the syntax :\n-for an ip address filter, follow these examples : 'ip_address == 192.126.0.12' or 'ip_address != 192.126.0.12'\n to choose if it's the source or destination, add '_src' or '_dst' like this : 'ip_address_src == 192.126.0.12'\n-for a TCP port, follow these examples : 'port == 2345' or 'port != 2345'\n to choose if it's the source or destination, add '_src' or '_dst' like this : 'port_src == 2345'";
-    printf("%s", syntax);
-    char str[256];
-    while (1) {
-        printf("Enter a filter (up to 33 characters) : ");
-        fgets(str, sizeof(str), stdin);
-        // Remove the newline character from the end of the string
-        str[strlen(str) - 1] = '\0';
-        
-        if (strcmp(str, "q") == 0)
-        {
-            return NULL;
-        }
-        
-        if (strlen(str) <= 33 && (strstr(str, "ip_address") != NULL || strstr(str, "port") != NULL || strstr(str, "none") != NULL)) {
-            printf("You entered: %s\n", str);
-            break;
-        }
-        else 
-        {
-            printf("Input must be up to 33 characters. Try again.\n");
-        }
-    }
-    
+    puts("Before typing your filter, the syntax of the filter is the following : 'filter_expression == value'.");
+    puts("Unfortunately, only a very limited choice of filter_expression is available ('ip_address' and 'port' to which you can add '_src' or '_dst' depending on the filter you want to apply).");
+    puts("Examples :\n-for an ip address filter : 'ip_address != 192.126.0.12' or 'ip_address_src == 192.126.0.12'");
+    puts("-for a TCP port filter : 'port != 2345' or 'port_dst == 2345'\n");
+
+    char* input = verif_input("Enter your filter : ");
+
     if (strcmp(str, "q") == 0)
             return NULL;
     if (strcmp(str, "none") == 0)
@@ -106,20 +89,230 @@ char *filter(frame *ptr)
         while (tmp)
         {
             tmp->print = 1;
+            tmp = tmp->suiv;
         }
     }
     else if (strstr(str, "ip_address") != NULL)
     {
-        return filter_ip(ptr);
+        filter_ip(input, ptr);
     }
 
     else if (strstr(str, "port"))
     {
-        return filter_port(ptr);
+        filter_port(input, ptr);
     }
-    return strdup(str);
+
+    return input;
 }
 
+void verif_input(char *prompt) {
+    while (1) {
+        char input[256];
+        printf("%s", prompt);
+        
+        fgets(input, 256, stdin);
+        input[strcspn(input, "\n")] = 0;    
+
+        regex_t ip_port_regex, q_none_regex;
+        regcomp(&ip_port_regex, "^(ip_address|port)(_src|_dst)? (==|!=) .*$", REG_EXTENDED);
+        regcomp(&q_none_regex, "^(q|none)$", REG_EXTENDED);
+
+        if (!regexec(&ip_port_regex, input, 0, NULL, 0)) {
+            regfree(&ip_port_regex);
+            regfree(&q_none_regex);
+            return strdup(input);
+        }
+
+        if (!regexec(&q_none_regex, user_input, 0, NULL, 0)) {
+            regfree(&ip_port_regex);
+            regfree(&q_none_regex);
+            return strdup(input);
+        } 
+        puts("Input doesn't respect the syntax.");
+    }
+}
+
+void filter_ip(char *str, frame *ptr)
+{
+    frame *tmp = ptr;
+    char *ip = strdup(&str[3]);
+    char *eq = strstr(str, "==");
+
+    while (tmp)
+    {
+        if (tmp->ip != NULL)
+        {
+            char *temp1 = hex_to_ip(tmp->ip->src_ip);
+            char *temp2 = hex_to_ip(tmp->ip->dest_ip);
+            if (eq != NULL)
+            {
+                if (strstr(str, "src"))   //ip_address_src  == ...
+                {  
+                    if (strcmp(ip, temp1) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else if (strstr(str, "dst"))  //ip_address_dst  == ...
+                {
+                    if (strcmp(ip, temp2) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else    //ip_address  == ...
+                {
+                    if (strcmp(ip, temp1) != 0 && strcmp(ip, temp2) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+            }
+            else    //ip_address... != ...
+            {
+                if (strstr(str, "src"))   //ip_address_src != ...
+                {
+                    if (strcmp(ip, temp1) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else if (strstr(str, "dst"))  //ip_address_src != ...
+                {
+                    if (strcmp(ip, temp2) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else{
+                    if (strcmp(ip, temp1) == 0 || strcmp(ip, temp2) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+            }
+            free(temp1);
+            free(temp2);
+        }
+        tmp = tmp->suiv; 
+    }
+}
+
+
+            
+void filter_port(char* str, frame *ptr)
+{
+    frame *tmp = ptr;
+    char *tcpport = strdup(&str[3]);
+    char *eq = strstr(str, "==");
+
+    while (tmp)
+    {
+        if (tmp->tcp != NULL)
+        {
+            char *temp1 = hexToDec_c(tmp->tcp_->src_port);
+            char *temp2 = hexToDec_c(tmp->tcp_->dst_port);
+            if (eq != NULL)
+            {
+                if (strstr(str, "src"))   //ip_address_src  == ...
+                {  
+                    if (strcmp(tcpport, temp1) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else if (strstr(str, "dst"))  //ip_address_dst  == ...
+                {
+                    if (strcmp(tcpport, temp2) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else    //ip_address  == ...
+                {
+                    if (strcmp(tcpport, temp1) != 0 && strcmp(tcpport, temp2) != 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+            }
+            else    //port... != ...
+            {
+                if (strstr(str, "src"))   //port_src != ...
+                {
+                    if (strcmp(tcpport, temp1) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else if (strstr(str, "dst"))  //port_src != ...
+                {
+                    if (strcmp(tcpport, temp2) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+                else{       //port != ...
+                    if (strcmp(tcpport, temp1) == 0 || strcmp(tcpport, temp2) == 0)
+                    {
+                        tmp->print = 0;
+                    }
+                    else
+                    {
+                        tmp->print = 1;
+                    }
+                }
+            }
+            free(temp1);
+            free(temp2);
+        }
+        tmp = tmp->suiv; 
+    }
+}
 
 void delete_frame(frame *fr)
 {
